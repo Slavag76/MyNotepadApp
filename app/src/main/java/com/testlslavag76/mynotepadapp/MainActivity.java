0e13db32d14f6cfddc9e5f7702bb3d1dfb141d87
SlavaGr <Slavag76@yandex.ru>
2021-06-17 14:34:03 +0300

    Context Menu

--- app/src/main/java/com/testlslavag76/mynotepadapp/MainActivity.java
+++ app/src/main/java/com/testlslavag76/mynotepadapp/MainActivity.java
@@ -1,9 +1,15 @@
 package com.testlslavag76.mynotepadapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import androidx.appcompat.widget.Toolbar;
 
 import com.testlslavag76.mynotepadapp.ui.NotepadListFragment;
 import com.testlslavag76.mynotepadapp.ui.details.NoteDetailsActivity;
@@ -19,6 +25,32 @@ public class MainActivity extends AppCompatActivity implements NotepadListFragme
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Toolbar toolbar = findViewById(R.id.toolbar);
+
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_appbar, menu);
+        return true;
+
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        int itemId = item.getItemId();
+
+        if (itemId == R.id.option_one){
+            Toast.makeText(this, "Option One Selected", Toast.LENGTH_LONG).show();
+            return true;
+        }
+
+
+
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
